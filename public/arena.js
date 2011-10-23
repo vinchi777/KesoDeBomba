e546f82b7d44873a3358a6fc4eda8a83b0706a28
Jude Calimbas <jude_calimbas@yahoo.com>
2011-10-23 09:57:05 +0800

    inde variable in arena.js

--- public/arena.js
+++ public/arena.js
@@ -2,11 +2,14 @@ function Arena(){
 var nrows = 12;
 var ncols = 12;
 this.index = new Array(nrows);
-
+ for(i=0; i<nrows; i++){
+   this.index[i] = new Array(ncols);
+  for(j=0; j<ncols; j++)
+   this.index[i][j] = "empty";
+ }
 	
 	 this.initialize = function(){
 		 for(i=1; i< nrows; i+=2){
-			this.index[i] = new Array(ncols);
 		    for(j=1; j < ncols; j+=2){
 		       this.index[i][j] = "block";
 			var ypos = 50 * j;
