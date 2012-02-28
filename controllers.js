2d68f9b41fbb2c8079e9fd04d736f4f99a427eee
Jude <jude_calimbas@yahoo.com>
2012-02-28 12:58:10 +0800

    register update

--- controllers.js
+++ controllers.js
@@ -15,15 +15,14 @@ function login(pg,conString,form,res,req){
 	});
 }
 function register(pg,conString,form,res){
- pg.connect(conString, function(err, client) {
-		client.query("SELECT * from users WHERE name=$1 ",[form.name], function(err, result) {
+	 pg.connect(conString, function(err, client) {
+		client.query("SELECT * from users WHERE name=$1",[form.name], function(err, result) {
 			if(result.rows.length >0){ 
-				delete req.session.name;
- 				res.render(__dirname + '/login.jade',{access:"username already exist"});
+				console.log(err);
+ 				res.render(__dirname + '/login.jade',{access:"username already exist"}); 
 			}
 			else{
 				client.query("INSERT INTO users(name,password) VALUES($1,$2)",[form.name,form.password]);
-				delete req.session.name; 
 				res.render(__dirname + '/login.jade',{access:"register complete"});
 			}
 		});
